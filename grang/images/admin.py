e26ff340f1be19a344b31cfe7d4f741c193cd8b3
Daesung <Daesung@Daesungui-MacBook-Pro.local>
2018-09-06 07:02:55 +0900

    finished db

--- grang/images/admin.py
+++ grang/images/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    
+    list_display_links = (
+        'location',
+        'caption',
+    )
+
+    search_fields = (
+        'location', #검색을 만들 수 있음! 미쳤다리~
+        'caption',
+    )
+
+    list_filter = (
+        'location',
+        'caption',
+    )
+
+    list_display = (
+        'file',
+        'location',
+        'caption',
+        'creator',
+        'created_at',
+        'updated_at',
+    )
+
+@admin.register(models.Like)
+class LikeAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
+
+
+@admin.register(models.Comment)
+class CommentAdmid(admin.ModelAdmin):
+    
+    list_display = (
+        'message',
+        'creator',
+        'image',
+        'created_at',
+        'updated_at',
+    )
--- grang/images/models.py
+++ grang/images/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from grang.users import models as user_models
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class TimeStampedModel(models.Model):
+
+     created_at = models.DateTimeField(auto_now_add=True) #처음 애드 되었을 때만 설치 1-20
+     updated_at = models.DateTimeField(auto_now=True)
+
+     class Meta:
+        abstract = True
+
+
+@python_2_unicode_compatible
+class Image(TimeStampedModel):
+
+    """ Image Model """
+
+    file = models.ImageField() #사진 올림
+    location = models.CharField(max_length=140)
+    caption = models.TextField()
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+
+
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption) 
+
+@python_2_unicode_compatible
+class Comment(TimeStampedModel): #댓글
+
+    """ Comment Model """
+
+    message = models.TextField() #대화에 이용 가능 ?
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.message
+
+@python_2_unicode_compatible
+class Like(TimeStampedModel):    
+
+    """ Like Model """ 
+
+    creator = models.ForeignKey(user_models.User, on_delete=models.PROTECT, null=True)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return 'User: {} - Image Caption: {}'.format(self.creator.username, self.image.caption)
+
+#class Cock(TimeStampedModel): #Cock 1-22#
+
+    """ Cock Model """
