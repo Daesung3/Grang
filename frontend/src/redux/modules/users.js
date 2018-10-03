8170c3c06058454744bb8a6c90b1574503667dea
Daesung <Daesung@Daesungui-MacBook-Pro.local>
2018-10-04 05:24:29 +0900

    Before node newest

---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,28 @@
+// imports
+
+// actions
+
+// action creator
+
+// intial state
+
+const intialState = {
+    isLoggedIn: localStorage.getItem("jwt") || false
+};
+
+// reducer
+
+function reducer(state = intialState, action){
+    switch(action.type){
+        default:
+         return state;
+    }
+}
+
+// reducer functions
+
+// exports
+
+// reducer export
+
+export default reducer;
