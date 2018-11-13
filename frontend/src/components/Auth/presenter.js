96be82254929419b0e7dc712d02dbaeaae457453
DAESUNG KIM <dskim4667@gmail.com>
2018-11-14 04:42:19 +0900

    frontend

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -5,57 +5,29 @@ import LoginForm from "components/LoginForm";
 import SignupForm from "components/SignupForm";
 
 const auth = (props, context) => (
-    <main className={styles.auth}>
-        <img id={styles.logo} src={require("images/logo.png")} alt="Logo" />
-        <ul className={styles.scrollNavi}>
-            <li><span><em>grang</em></span></li>
-            <li><span onClick={props.signupAction}><em>start</em></span></li>
-            <li><span onClick={props.loginAction}><em>login</em></span></li>
-        </ul>
-        <div className={`${styles.whiteBox} ${styles.formBox}`}>
-                {props.action === "login" && <LoginForm />}
-                {props.action === "signup" && <SignupForm />}
-        </div>
-        <div className={styles.intro}>
-            <img id={styles.heart} src={require("images/heart.png")} alt="checkout our app. is cool" />
-            <h2>그랑은 사람들과 동그랗게 어울려 살려는 순 우리말입니다.</h2>
-        </div>
-        <figure className={styles.time}>
-            <p>
-                <span>00</span> : <span>00</span> : <span>00</span>
-            </p>
-        </figure>
-        {/* <section> */}
-           {/*  <article className={styles.start}>
-                <h2>그랑 시작하기</h2>
-                {props.action === "signup" && <signupform />}
-                {props.action === "login" && <loginform />}
-            </article>
-            <div className={styles.whitebox}>
-                {props.action === "signup" && (
-                    <p>
-                        have an account?{" "}
-                        <span className={styles.changelink} onclick={props.changeaction}>
-                            log in
-                        </span>
-                    </p>
-                )}
-                {props.action === "login" && (
-                    <p>
-                        don't have an account?{" "}
-                        <span className={styles.changelink} onclick={props.changeaction}>
-                            sign up
-                        </span>
-                    </p>
-                    </div>
-                )}
-             */}
-       {/*  </section> */}
-    </main>
-        );
-     
+  <main className={styles.auth}>
+    <div className={styles.background} />
+    <img id={styles.logo} src={require("images/logo.png")} alt="Logo" />
+    <ul className={styles.scrollNavi}>
+      <li>
+        <button onClick={props.signupAction}>시작하기</button>
+      </li>
+      <li>
+        <button onClick={props.loginAction}>로그인</button>
+      </li>
+    </ul>
+    <div className={`${styles.whiteBox} ${styles.formBox}`}>
+      {props.action === "login" && <LoginForm />}
+      {props.action === "signup" && <SignupForm />}
+    </div>
+    <div className={styles.intro}>
+      <h2>그랑은 사람들과 동그랗게 어울려 살려는 순 우리말입니다.</h2>
+    </div>
+  </main>
+);
+
 auth.contexttypes = {
-            t: PropTypes.func.isrequired
-    };
-    
+  t: PropTypes.func.isrequired
+};
+
 export default auth;
--- frontend/src/components/Navigation/presenter.js
+++ frontend/src/components/Navigation/presenter.js
@@ -5,55 +5,43 @@ import { Link } from "react-router-dom";
 import styles from "./styles.scss";
 
 const Navigation = (props, context) => (
-    <div className={styles.navigation}>
-        <div className={styles.inner}>
-            <div className={styles.column}>
-                <Link to="/">
-                <img
-                 src={require("images/logo.png")}
-                 className={styles.logo}
-                 alt={context.t("logo")}
-                 />
-                 </Link>
-            </div>
-            <div className={styles.column}>
-                <div className={styles.navIcon}>
-                    <Link to="/changecategory" alt="Change_Category">
-                        <Ionion icon="ios-sync" fontSize="28px" color="#FCD15A"/>
-                    </Link>
-                </div>
-                <div className={styles.navIcon}>
-                    <Link to="/search" alt="Search">
-                        <Ionion icon="md-search" fontSize="28px" color="#FCD15A"/>
-                    </Link>
-                </div>
-                <div className={styles.navIcon}>
-                    <Link to="/add">
-                        <Ionion icon="md-add" fontSize="28px" color="#FCD15A" />
-                    </Link>
-                </div>
-                <div className={styles.navIcon}>
-                    <Link to="/profile">
-                        <Ionion icon="md-person" fontSize="32px" color="#FCD15A" />
-                    </Link>
-                </div>
-                <div className={styles.navIcon} onClick={props.onClickLogout}>
-                    <Ionion icon="md-log-out" fontSize="28px" color="#FCD15A" />
-                </div>
-            </div>
+  <div className={styles.navigation}>
+    <div className={styles.inner}>
+      <div className={styles.column}>
+        <Link to="/">
+          <img
+            src={require("images/logo.png")}
+            className={styles.logo}
+            alt={context.t("logo")}
+          />
+        </Link>
+      </div>
+      <div className={styles.column}>
+        <div className={styles.navIcon}>
+          <Link to="/changecategory" alt="Change_Category">
+            <Ionion icon="ios-sync" fontSize="32px" color="black" />
+          </Link>
         </div>
-            
-
+        <div className={styles.navIcon}>
+          <Link to="/add">
+            <Ionion icon="md-add" fontSize="28px" color="black" />
+          </Link>
+        </div>
+        <div className={styles.navIcon}>
+          <Link to="/profile">
+            <Ionion icon="md-person" fontSize="32px" color="black" />
+          </Link>
+        </div>
+        <div className={styles.navIcon} onClick={props.onClickLogout}>
+          <Ionion icon="md-log-out" fontSize="28px" color="black" />
+        </div>
+      </div>
     </div>
+  </div>
 );
 
-
-
-
 Navigation.contextTypes = {
-    t: PropTypes.func.isRequired //context는 번역을 위한 것
-}
-
-
+  t: PropTypes.func.isRequired //context는 번역을 위한 것
+};
 
 export default Navigation;
